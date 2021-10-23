0524231b894128951d45c5ddb2db5f76ab5a0630
Umuthan Özel <ozelumuthan@gmail.com>
2021-10-23 13:00:07 +0300

    Project initialization

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+ 
+
+    void Start()
+    {
+        for(int i = 0; i < 5; i++) 
+        {
+            continue;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
